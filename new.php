bbfa9d8ecb0ca87ce478d2fb59f57db055b3675e
Brandon-Crosby <53662951+Brandon-Crosby@users.noreply.github.com>
2019-11-01 12:31:00 -0400

    Update new.php

--- new.php
+++ new.php
@@ -1,4 +1,3 @@
-
 <?php
 require 'inc/functions.php';
 include 'inc/header.php';
@@ -8,23 +7,23 @@ if($_SERVER['REQUEST_METHOD'] =='POST'){
     $id= trim(filter_input(INPUT_POST,'id',FILTER_SANITIZE_NUMBER_INT));  //entry_id required????
     $title= trim(filter_input(INPUT_POST,'title',FILTER_SANITIZE_STRING));
     $date= trim(filter_input(INPUT_POST,'date',FILTER_SANITIZE_STRING));
-    $time_spent= trim(filter_input(INPUT_POST,'timeSpent',FILTER_SANITIZE_NUMBER_INT));
+    $time_spent= trim(filter_input(INPUT_POST,'timeSpent',FILTER_SANITIZE_STRING));
     $learned= trim(filter_input(INPUT_POST,'whatILearned',FILTER_SANITIZE_STRING));
     $resources= trim(filter_input(INPUT_POST,'ResourcesToRemember',FILTER_SANITIZE_STRING));
 
-
+//DATE CHECK FORMAT & IF VALID
     $dateMatch= explode('-',$date);
 
  if (empty($title)||empty($date)) {
       $error_message = 'Please fill in required fields: Title and/or Date';
-/*}elseif(count($dateMatch) != 3
+    }elseif(count($dateMatch) != 3
          ||strlen($dateMatch[0])!= 4
          ||strlen($dateMatch[1])!= 2
          ||strlen($dateMatch[2])!= 2
-         ||!checkdate($dateMatch[0],$dateMatch[1],$dateMatch[2])){
+         ||!checkdate($dateMatch[1],$dateMatch[2],$dateMatch[0])){
     $error_message = "Invalid Date";
     echo 'Please fill in required fields: Title and/or Date(YYYY-MM-DD)';
-*/}else{
+  }else{
     if (add_entry($title,$date,$time_spent,$learned,$resources,$id)){
     echo 'Journal Entry added.';
     header('Location: index.php');
@@ -61,15 +60,15 @@ if($_SERVER['REQUEST_METHOD'] =='POST'){
                     ?>
                     <form method="POST">
                         <label for="title"> Title</label>
-                        <input id="title" type="text" name="title" placeholder="Title"><br>
+                        <input id="title" type="text" name="title" placeholder="Title" value="<?php echo htmlspecialchars($title); ?>"><br>
                         <label for="date">Date</label>
-                        <input id="date" type="date" value ="" name="date" placeholder="YYYY-MM-DD" value= "<?php htmlspecialchars($date); ?>"><br>
+                        <input id="date" type="date" name="date" placeholder="YYYY-MM-DD" value= "<?php echo htmlspecialchars($date); ?>"><br>
                         <label for="time-spent"> Time Spent</label>
-                        <input id="time-spent" type="text" name="timeSpent" placeholder="1 hour" value= "<?php htmlspecialchars($time_spent); ?>"><br>
+                        <input id="time-spent" type="text" name="timeSpent" placeholder="1 hour" value= "<?php echo htmlspecialchars($time_spent); ?>"><br>
                         <label for="what-i-learned">What I Learned</label>
-                        <textarea id="what-i-learned" rows="5" name="whatILearned" placeholder="Today I learned..." value = "<?php htmlspecialchars($learned); ?>"></textarea>
+                        <textarea id="what-i-learned" rows="5" name="whatILearned" placeholder="Today I learned..." value = ""><?php echo htmlspecialchars($learned); ?></textarea>
                         <label for="resources-to-remember">Resources to Remember</label>
-                        <textarea id="resources-to-remember" rows="5" name="ResourcesToRemember" placeholder="Treehouse.com" value = "<?php htmlspecialchars($resources); ?>"> </textarea>
+                        <textarea id="resources-to-remember" rows="5" name="ResourcesToRemember" placeholder="Treehouse.com" value = ""><?php echo htmlspecialchars($resources); ?> </textarea>
                         <input type="submit" value="Publish Entry" class="button">
                         <a href="index.php" class="button button-secondary">Cancel</a>
                     </form>
